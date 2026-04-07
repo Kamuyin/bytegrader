e287e6d9270be7cc5240f41cedb6b2b385f1dd41
Kamuyin <56199686+Kamuyin@users.noreply.github.com>
2026-04-07 22:47:44 +0200

    fix(systemd): Added import shim for systemd-executor

--- bytegrader/autograde/executors/__init__.py
+++ bytegrader/autograde/executors/__init__.py
@@ -4,6 +4,12 @@ __all__ = ["SimpleExecutor"]
 # _all__ = []
 
 try:
+    from .systemd import SystemdExecutor, SystemdExecutorConfig
+    __all__.extend(["SystemdExecutor", "SystemdExecutorConfig"])
+except ImportError:
+    pass
+
+try:
     from .wasm import WasmExecutor
     __all__.append("WasmExecutor")
 except ImportError:
---
+++ bytegrader/autograde/executors/systemd.py
@@ -0,0 +1,3 @@
+from bytegrader_systemd import SystemdExecutor, SystemdExecutorConfig
+
+__all__ = ["SystemdExecutor", "SystemdExecutorConfig"]
